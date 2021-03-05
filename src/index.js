4f25476e60c82236b6f107fa2af472da7f97e130
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2021-03-05 10:57:07 -0500

    fixing broken placeholder link

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import store from './store';
+// import {BrowserRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
 	<React.StrictMode>
